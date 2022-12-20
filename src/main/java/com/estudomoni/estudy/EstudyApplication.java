92acda3cc205e7e6d972462b331eee73bf6b949c
monielle <monielle@gmail.com>
2022-12-20 18:54:23 -0300

    Project created

---
+++ src/main/java/com/estudomoni/estudy/EstudyApplication.java
@@ -0,0 +1,13 @@
+package com.estudomoni.estudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudyApplication.class, args);
+	}
+
+}
